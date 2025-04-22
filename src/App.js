0e326f8d6089fa784ae3cdc94acd8651a3e26e04
aneelammar <aneel01ammar@gmail.com>
2025-04-22 12:27:44 +0300

    final version

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
 import TicketList from "./components/TicketList";
+import { sortTickets } from "./utilites/sortingUtilites";
 
 function App() {
-  const initialState = { tickets: [], editingTicket: null };
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low",
+  };
 
   const [state, dispatch] = useReducer(ticketReducer, initialState);
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
 
   return (
     <div className="App">
@@ -24,8 +30,18 @@ function App() {
           <div className="results">
             <h2> All Tickets</h2>
 
+            <select
+              value={state.sortPreference}
+              onChange={(e) =>
+                dispatch({ type: "SET_SORTING", payload: e.target.value })
+              }
+            >
+              <option value={"High to Low"}>High to Low</option>
+              <option value={"Low to High"}>Low to High</option>
+            </select>
+
             <TicketList
-              tickets={state.tickets}
+              tickets={sortedTickets}
               dispatch={dispatch}
             ></TicketList>
           </div>
--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -40,7 +40,11 @@ export default function ticketReducer(state, action) {
         ...state,
         editingTicket: null,
       };
-
+    case "SET_SORTING":
+      return {
+        ...state,
+        sortPreference: action.payload,
+      };
     default:
       return state;
   }
